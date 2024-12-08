acbf43ba022192790fbd8158731deddaaa9ba274
Karo <karo.wolna@gmail.com>
2024-12-08 18:34:50 +0100

    Created a new file for DB

--- DB_Connect.py
+++ DB_Connect.py
@@ -1,2 +1,8 @@
-import sqlite3
-print(sqlite3.sqlite_version)  # Gibt die SQLite-Version zurück
+try:
+    import sqlite3
+except ImportError:
+    print("SQLite3 ist nicht installiert oder verfügbar.")
+    print("Versuche, es mit folgendem Befehl zu installieren: pip install pysqlite3")
+else:
+    # Wenn sqlite3 verfügbar ist, tue nichts
+    pass
