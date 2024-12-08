c89229010ae9e9849fbae1a8119a471e82a60482
KarolinaWolna <158358410+KarolinaWolna@users.noreply.github.com>
2024-12-08 21:26:45 +0100

    Delete DB_Connect.py
    File has to be moved in Finanzplaner branch.

--- DB_Connect.py
+++
@@ -1,8 +0,0 @@
-try:
-    import sqlite3
-except ImportError:
-    print("SQLite3 ist nicht installiert oder verfügbar.")
-    print("Versuche, es mit folgendem Befehl zu installieren: pip install pysqlite3")
-else:
-    # Wenn sqlite3 verfügbar ist, tue nichts
-    pass
